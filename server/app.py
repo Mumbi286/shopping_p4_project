fe8ccfda53740ac5c86c408dd7f0010d24fb74ee
akamnovelakwa-ship-it <akam.akwa@student.moringaschool.com>
2026-01-14 13:37:51 +0300

    add changes

---
+++ server/app.py
@@ -0,0 +1,15 @@
+from fastapi import fastAPI,depends,HTTPException,query
+from sqlalchemy.import(
+    create_engine,Column,Integer,String,
+    Boolean,ForeignKey,DateTime,Decimal, Text
+)
+from sqlalchemy.orm import declarative_base,sessionmaker,relationship,Session
+from datetime import datetime
+
+
+# DATABASE CONFIG
+# -----------------------------
+DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/shopping_cart"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine,autoflush=False)
+Base = declarative_base()
