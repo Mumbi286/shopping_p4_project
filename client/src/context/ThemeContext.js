320a13c836b356b8c4acadcb05c4c659adbf9454
jamesmutune <tunekivasu01@gmail.com>
2026-01-14 05:28:40 +0300

    setup light and dark mode themes with ThemeContext

---
+++ client/src/context/ThemeContext.js
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+import { lightTheme, darkTheme } from "../styles/theme";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(lightTheme);
+
+  const toggleTheme = () => {
+    setTheme(prev => (prev === lightTheme ? darkTheme : lightTheme));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
