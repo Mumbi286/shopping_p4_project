36596f37e3947d1b232b38385a9918bfcea3a8e3
Christine Mumbi <christinemumbi286@gmail.com>
2026-01-15 23:07:18 +0300

    Adds the middleware

--- server/app.py
+++ server/app.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import models
 from database import engine
 from routers import cart, auth
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 
